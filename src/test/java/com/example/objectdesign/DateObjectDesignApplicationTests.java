d030dc133c70096f68c8e506e5bf62516774e969
woonys <wodns1324@naver.com>
2022-09-27 22:51:14 +0900

    first commit: Circular dependency

---
+++ src/test/java/com/example/objectdesign/DateObjectDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.objectdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateObjectDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
